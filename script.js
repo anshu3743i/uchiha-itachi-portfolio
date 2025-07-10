4dbd7a2164dad9a04e70bf975fc9c21befbf1a47
ash sinha <anshusinha036@gmail.com>
2025-07-10 23:49:22 +0530

    main1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+// function Change() {
+//   let h1 = document.querySelector("#text");
+//   let bt = document.querySelector("#btn");
+//   let sec = document.querySelector("#section");
+//   h1.style.color = "white";
+//   sec.style.backgroundColor = "black";
+//   bt.style.display = "none";
+//   let jj = document.querySelector("#ji");
+//   jj.style.display = "block";
+// }
+ 
+  function Change() {
+    document.documentElement.classList.toggle("dark");
+  }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        michroma: ['Michroma', 'sans-serif'],
-        chewy: ['Chewy', 'cursive'],
-        bubblegum: ['Bubblegum Sans', 'cursive'],  // added here
+        michroma: ["Michroma", "sans-serif"],
+        chewy: ["Chewy", "cursive"],
+        bubblegum: ["Bubblegum Sans", "cursive"], // added here
       },
     },
   },
